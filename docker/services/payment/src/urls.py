2c6888efb354a1dfb50e4e54cd0c97fa856a7e71
Hugo Brilhante <hugob@airg.com>
2024-01-30 18:36:50 -0300

    Ruff in files

--- docker/services/payment/src/urls.py
+++ docker/services/payment/src/urls.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("src.core.urls"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("src.core.urls"))]
