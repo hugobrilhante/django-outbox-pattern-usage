2a9bd43cea1072f819ad555e44ddf418ec1b94f2
Hugo Brilhante <hugob@airg.com>
2024-01-30 18:09:55 -0300

    Move to docker folder

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,7 +7,3 @@ echo "Stopping  services..."
 source scripts/run.sh down order stock payment rabbitmq kong kong-db > /dev/null 2>&1
 
 docker volume prune -f > /dev/null 2>&1
-
-rm -rf services/order/logs/*
-rm -rf services/stock/logs/*
-rm -rf services/payment/logs/*
